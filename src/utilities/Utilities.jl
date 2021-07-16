46be28a99d7ee429b9f34319f9f5c465c5242122
Stef Kuypers <stef@happonomy.org>
2021-07-16 15:21:12 +0200

    Added exports for interval test functions.

--- src/utilities/Utilities.jl
+++ src/utilities/Utilities.jl
@@ -6,6 +6,7 @@ export LeftInterval, RightInterval, ClosedInterval
 
 include("functions.jl")
 export extract, delete_element!
+export is_left_closed, is_right_closed, is_left_open, is_right_open, is_left_unbounded, is_right_unbounded
 
 include("constants.jl")
 export INF, CUR_MIN, CUR_MAX
