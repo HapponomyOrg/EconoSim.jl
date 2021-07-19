c394735d137481acaee103c400222d918a57bb27
Stef Kuypers <stef@happonomy.org>
2021-07-19 12:01:05 +0200

    Added customisable comments fields for transaction logs.

--- src/models/sumsy_model.jl
+++ src/models/sumsy_model.jl
@@ -2,16 +2,20 @@ using Agents
 
 @enum CommonGoodMode none fixed_contribution on_demand_reserves
 
+is_sumsy_active(actor::Actor) = is_sumsy_active(actor.balance)
+process_sumsy!(sumsy::SuMSy, actor::Actor, step::Int) = process_sumsy!(sumsy, actor.balance, step)
+
 function create_sumsy_model(sumsy::SuMSy,
                             common_good_mode::CommonGoodMode = none,
                             contribution_free::Real = 0,
                             contribution_tiers::Vector{T} = Vector{T}(),
                             interval::Int = sumsy.interval) where {T <: Tuple{Real, Real}}
     model = create_econo_model()
+    model.properties[:common_good_mode] = common_good_mode
 
     if common_good_mode != none
-        contribution = SuMSy(0, contribution_free, contribution_tiers, sumsy.interval, 0)
-
+        contribution = SuMSy(0, contribution_free, contribution_tiers, interval, 0)
+        model.properties[:contribution] = contribution
     end
 
     return model
@@ -24,3 +28,21 @@ function create_susmsy_model(sumsy::SuMSy,
                             interval::Int = sumsy.interval)
     return create_sumsy_model(sumsy, common_good_mode, contribution_free, [(0, contribution)], interval)
 end
+
+function sumsy_model_step!(model)
+    econo_model_step!(model)
+
+    for actor in allagents(model)
+        process_sumsy!(sumsy, actor, model.step)
+
+        if model.common_good_mode != none
+            contribution = model.contribution
+
+        end
+    end
+end
+
+function make_sumsy!(model, actor::Actor; guaranteed_income::Bool = true)
+end
+
+sumsy_step!(model, steps::Integer = 1) = econo_step!(model, steps)
