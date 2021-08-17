bfa10ad49f31d51517a003424ae2cc60c44e3514
Stef Kuypers <stef@happonomy.org>
2021-08-17 14:52:13 +0200

    Updated exports

--- src/models/Models.jl
+++ src/models/Models.jl
@@ -3,3 +3,7 @@ export create_econo_model, econo_step!, econo_model_step!
 export get_step, get_price, set_price!
 
 include("sumsy_model.jl")
+export no_contribution, fixed_contribution, on_demand_contribution
+export is_sumsy_active, calculate_demurrage, sumsy_balance
+export create_sumsy_model, sumsy_step!, sumsy_model_step!
+export request_contribution!, CONTRIBUTION_SHORTAGE
