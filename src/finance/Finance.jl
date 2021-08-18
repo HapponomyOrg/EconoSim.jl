efa9a23dfb00fe917a8481b9911074595447761b
Stef Kuypers <stef@happonomy.org>
2021-08-18 14:43:59 +0200

    Updated exports

--- src/finance/Finance.jl
+++ src/finance/Finance.jl
@@ -1,6 +1,7 @@
 include("balance.jl")
 export EntryType, BalanceEntry, Balance, AtomicTransaction, Transaction
 export EQUITY, asset, liability
+export has_asset, has_liability
 export clear!, min_balance!, min_asset!, min_liability!, min_balance, min_asset, min_liability
 export validate, assets, liabilities, asset_value, assets_value, liability_value, liabilities_value, liabilities_net_value, equity
 export book_asset!, book_liability!
@@ -8,10 +9,11 @@ export transfer!, transfer_asset!, transfer_liability!
 export queue_transfer!, queue_asset_transfer!, queue_liability_transfer!, execute_transfers!
 
 include("sumsy.jl")
-export SUMSY_DEP
-export SuMSy, DemTiers, make_tiers, dem_tier
+export SUMSY_DEP, SUMSY_DEBT
+export SuMSy, DemTiers, DemSettings, make_tiers, NO_DEM_TIERS
+export SuMSyOverrides
 export calculate_demurrage, process_sumsy!, sumsy_balance, sumsy_transfer!
-export set_sumsy_active!, is_sumsy_active, set_sumsy!, set_seed!, get_seed, set_guaranteed_income!, get_guaranteed_income, set_dem_free!, get_dem_free, transfer_dem_free!, set_dem_tiers, get_dem_tiers
+export set_sumsy_active!, is_sumsy_active, set_sumsy_overrides!, set_seed!, get_seed, set_guaranteed_income!, get_guaranteed_income, set_initial_dem_free!, get_initial_dem_free, get_dem_free, transfer_dem_free!, set_dem_tiers, get_dem_tiers
 
 include("debt.jl")
 export DEPOSIT, DEBT
