a2b4118a02e0237ae4b5dae73eab8676a870e26f
Stef Kuypers <stef@happonomy.org>
2021-08-18 16:10:40 +0200

    Updated tests

--- test/models_test.jl
+++ test/models_test.jl
@@ -9,53 +9,57 @@ using Agents
 
     run!(model, actor_step!, sumsy_model_step!, 40)
 
-    @test sumsy_balance(actor, model.sumsy) == 8350
+    @test sumsy_balance(actor, model) == 8350
     @test !(:contribution_balance in keys(model.properties))
 end
 
 @testset "SuMSy contribution - fixed" begin
     model = create_sumsy_model(SuMSy(2000, 5000, 0.1, 10, seed = 500),
-                        fixed_contribution, 0, 0.1)
+                        fixed_contribution,
+                        contribution_free = 0,
+                        contribution_tiers = 0.1)
     actor = Actor()
     add_agent!(actor, model)
 
     run!(model, actor_step!, sumsy_model_step!, 40)
 
-    @test sumsy_balance(actor, model.sumsy) == 7160
-    @test sumsy_balance(model.contribution_balance, model.sumsy) == 1257.5
+    @test sumsy_balance(actor, model) == 7160
+    @test sumsy_balance(model.contribution_balance, model) == 1257.5
 end
 
 @testset "SuMSy contribution - on demand" begin
     model = create_sumsy_model(SuMSy(2000, 5000, 0.1, 10, seed = 500),
-                        on_demand_contribution, 0, 0.1)
+                        on_demand_contribution,
+                        contribution_free = 0,
+                        contribution_tiers = 0.1)
     actor = Actor()
     add_agent!(actor, model)
 
     request_contribution!(model, 100)
     sumsy_step!(model, 10)
 
-    @test sumsy_balance(actor, model.sumsy) == 2500
-    @test sumsy_balance(model.contribution_balance, model.sumsy) == 0
+    @test sumsy_balance(actor, model) == 2500
+    @test sumsy_balance(model.contribution_balance, model) == 0
     @test model.contribution_shortage == 100
 
     request_contribution!(model, 100)
     sumsy_step!(model, 10)
 
-    @test sumsy_balance(actor, model.sumsy) == 4400
-    @test sumsy_balance(model.contribution_balance, model.sumsy) == 100
+    @test sumsy_balance(actor, model) == 4400
+    @test sumsy_balance(model.contribution_balance, model) == 100
     @test model.contribution_shortage == 0
 
     request_contribution!(model, 500)
     sumsy_step!(model, 10)
 
-    @test sumsy_balance(actor, model.sumsy) == 5960
-    @test sumsy_balance(model.contribution_balance, model.sumsy) == 540
+    @test sumsy_balance(actor, model) == 5960
+    @test sumsy_balance(model.contribution_balance, model) == 540
     @test model.contribution_shortage == 60
 
     request_contribution!(model, 1000)
     sumsy_step!(model, 10)
 
-    @test sumsy_balance(actor, model.sumsy) == 7268
-    @test sumsy_balance(model.contribution_balance, model.sumsy) == 1136
+    @test sumsy_balance(actor, model) == 7268
+    @test sumsy_balance(model.contribution_balance, model) == 1136
     @test model.contribution_shortage == 404
 end
