b787e73bef841056434fdb2b6cf237e18c5712c3
Stef Kuypers <stef@happonomy.org>
2022-02-04 11:09:51 +0100

    Added model behaviors to model

--- src/models/econo_model.jl
+++ src/models/econo_model.jl
@@ -1,12 +1,28 @@
 using Agents
 
-function create_econo_model()
+function behavior_vector(behaviors::Union{Nothing, Function, Vector{Function}})
+    if behaviors isa Function
+        return Vector{Function}([behaviors])
+    elseif behaviors isa Vector
+        return Vector{Function}(behaviors)
+    else
+        return Vector{Function}()
+    end
+end
+
+function create_econo_model(model_behaviors::Union{Nothing, Function, Vector{Function}} = nothing)
     properties = Dict{Symbol, Any}()
     properties[:step] = -1
     properties[:prices] = Dict{Blueprint, Price}()
+    properties[:model_behaviors] = behavior_vector(model_behaviors)
 
-    model = ABM(Actor, properties = properties)
+    return ABM(Actor, properties = properties)
 end
+
+has_model_behavior(model, behavior::Function) = behavior in model.model_behaviors
+add_model_behavior!(model, behavior::Function) = (push!(model.model_behaviors, behavior); model)
+delete_model_behavior!(model, behavior::Function) = (delete_element!(model.model_behaviors, behavior); model)
+clear_model_behaviors(model) = (empty!(model.model_behaviors); model)
 
 get_step(model) = model.step
 
@@ -16,13 +32,15 @@ set_price!(model, bp::Blueprint, price::Price) = (model.prices[bp] = price; mode
 function econo_model_step!(model)
     model.step += 1
 
-    for actor in allagents(model)
-        for behavior in actor.model_behaviors
-            behavior(model, actor)
-        end
+    for behavior in model.model_behaviors
+        behavior(model)
     end
 end
 
 function econo_step!(model, steps::Integer = 1)
     step!(model, actor_step!, econo_model_step!, steps, false)
+end
+
+function run_econo_model!(model, steps, kwargs...)
+    run!(model, actor_step!, econo_model_step!, steps, agents_first = false; kwargs...)
 end
