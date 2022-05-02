1a4f84badd69361e631f9b6b37a52d777e1b9f58
Stef Kuypers <stef@happonomy.org>
2022-05-02 16:22:14 +0200

    Added comments. Added actors_first Bool parameter to econo_step!() and run_economo_model!() functions.

--- src/models/econo_model.jl
+++ src/models/econo_model.jl
@@ -1,5 +1,13 @@
 using Agents
 
+"""
+    behavior_vector(behaviors::Union{Nothing, Function, Vector{Function}})
+
+Create a Vector of functions based on the input.
+If nothing is passed, an empty vector is returned.
+If a function is passed, a vector with one element is returned.
+If a vector is passed, the vector itself is returned.
+"""
 function behavior_vector(behaviors::Union{Nothing, Function, Vector{Function}})
     if behaviors isa Function
         return Vector{Function}([behaviors])
@@ -10,6 +18,12 @@ function behavior_vector(behaviors::Union{Nothing, Function, Vector{Function}})
     end
 end
 
+"""
+    create_econo_model(model_behaviors::Union{Nothing, Function, Vector{Function}} = nothing)
+
+Create a default model with 0 or more model behavior functions.
+Each cycle the model runs, all model behavior functions are called in order.
+"""
 function create_econo_model(model_behaviors::Union{Nothing, Function, Vector{Function}} = nothing)
     properties = Dict{Symbol, Any}()
     properties[:step] = -1
@@ -37,10 +51,10 @@ function econo_model_step!(model)
     end
 end
 
-function econo_step!(model, steps::Integer = 1)
-    step!(model, actor_step!, econo_model_step!, steps, false)
+function econo_step!(model, steps::Integer = 1, actors_first::Bool = false)
+    step!(model, actor_step!, econo_model_step!, steps, actors_first)
 end
 
-function run_econo_model!(model, steps::Integer; kwargs...)
-    run!(model, actor_step!, econo_model_step!, steps, agents_first = false; kwargs...)
+function run_econo_model!(model, steps::Integer, actors_first = false; kwargs...)
+    run!(model, actor_step!, econo_model_step!, steps, agents_first = actors_first; kwargs...)
 end
