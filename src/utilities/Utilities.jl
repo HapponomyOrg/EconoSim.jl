0565c3cec3ef97a3b38a4053f8debb9f0d4e60ad
Stef Kuypers <stef@happonomy.org>
2022-05-12 13:16:12 +0200

    Added export of CUR_0

--- src/utilities/Utilities.jl
+++ src/utilities/Utilities.jl
@@ -9,4 +9,4 @@ export extract, delete_element!
 export is_left_closed, is_right_closed, is_left_open, is_right_open, is_left_unbounded, is_right_unbounded
 
 include("constants.jl")
-export INF, CUR_MIN, CUR_MAX
+export INF, CUR_MIN, CUR_MAX, CUR_0
