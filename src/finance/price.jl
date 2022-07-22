85df1a75c27e6f426897edd221b34cd3eaaf1967
Stef Kuypers <stef@happonomy.org>
2022-07-22 14:07:19 +0200

    Price now uses Currency. Introduced main currency field.

--- src/finance/price.jl
+++ src/finance/price.jl
@@ -1,25 +1,42 @@
+using Todo
+
 import Base: +, -, *, /, <, >, <=, >=, ==, max, min
 
 """
     Price - a composite price consisting of one or more price components, each associated with a specific balance entry.
 """
 struct Price
-    components::Dict{BalanceEntry, Float64}
-    precision::Int64
-    Price(;precision::Integer = 2) = new(Dict{BalanceEntry, Float64}(), precision)
+    components::Dict{BalanceEntry, Currency}
+    main_currency::BalanceEntry
+    Price(components::Dict{BalanceEntry, <:Real}, main_currency::BalanceEntry) = new(Dict{BalanceEntry, Currency}(components), main_currency)
 end
 
-function Price(components::Dict{BalanceEntry, <:Real}; precision::Integer = 2)
-    price = Price(precision = precision)
+function Price(main_currency::BalanceEntry)
+    Price(Dict{BalanceEntry, Currency}(), main_currency)
+end
 
-    for entry in keys(components)
-        price.components[entry] = round(components[entry], digits = precision)
+function Price(components::AbstractVector{<:Pair{BalanceEntry, <:Real}}, main_currency::BalanceEntry = components[1][1])
+    Price(Dict{BalanceEntry, Real}(components), main_currency)
+end
+
+"""
+    ExchangeRates - all available exchange rates.
+        Conversion between currencies cooresponding to BalanceEntries are only possible if the exchangerate exists. Exchange rates are not symmetrical.
+        Conversion to the main currency is always possible. When no entry for conversion to the main currency is present, parity is assumed.
+"""
+ExchangeRates = Dict{Tuple{BalanceEntry, BalanceEntry}, Currency}
+
+function get_exhange_rate(exchange_rates::ExchangeRates, from::BalanceEntry, to::BalanceEntry, main_currency::BalanceEntry)
+    from_to = (from, to)
+
+    if haskey(exchange_rates, from_to)
+        return exchange_rates[from_to]
+    elseif to == main_currency
+        return Currency(1)
+    else
+        return Currency(0)
     end
-
-    return price
 end
-
-Price(components::AbstractVector{<:Pair{BalanceEntry, <:Real}}; precision::Integer = 2) = Price(Dict{BalanceEntry, Float64}(components), precision = precision)
 
 Base.isempty(price::Price) = isempty(price.components)
 Base.empty(price::Price) = empty(price.components)
@@ -27,13 +44,13 @@ Base.empty!(price::Price) = empty!(price.components)
 Base.keys(price::Price) = keys(price.components)
 Base.values(price::Price) = values(price.components)
 Base.getindex(price::Price, index::BalanceEntry) = price.components[index]
-Base.setindex!(price::Price, amount::Real, index::BalanceEntry) = (price.components[index] = round(amount, digits = price.precision))
+Base.setindex!(price::Price, amount::Real, index::BalanceEntry) = (price.components[index] = amount)
 
 function apply_op(price::Price, x::Real, op)
-    new_price = Price(precision = price.precision)
+    new_price = Price(price.main_currency)
 
     for entry in keys(price)
-        new_price[entry] = round(eval(op)(price[entry], x), digits = price.precision)
+        new_price[entry] = eval(op)(price[entry], x)
     end
 
     return new_price
@@ -44,7 +61,7 @@ function apply_op(p1::Price, p2::Price, op)
     price = Price(precision = max(p1.precision, p2.precision))
 
     for entry in entries
-        price[entry] = round(eval(op)(p1[entry], p2[entry]), digits = price.precision)
+        price[entry] = round(eval(op)(p1[entry], p2[entry]))
     end
 
     return price
@@ -65,16 +82,17 @@ end
 
 Base.:/(price::Price, x::Real) = apply_op(price, x, :/)
 
-function purchases_available(balance::Balance, price::Price, units::Integer)
-    max_available = INF
+todo"Implement with taking possible exchanges between currencies into account"
+function purchases_available(balance::Balance, price::Price, units::Integer; exchange_rates = ExchangeRates())
+    max_available = asset_value(balance, price.main_currency) / price[price.main_currency]
 
     for entry in keys(price)
         if price[entry] != 0
-            max_available = min(max_available, round(asset_value(balance, entry) / price[entry], RoundDown))
+            max_available = min(max_available, asset_value(balance, entry) / price[entry])
         end
     end
 
-    return min(units, Integer(max_available))
+    return min(units, Integer(round(Float64(max_available), RoundDown)))
 end
 
 """
