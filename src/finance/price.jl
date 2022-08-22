edc5bef1b669ebe0f4458e285b9d08ed95dba25c
Stef Kuypers <stef@happonomy.org>
2022-08-22 13:59:59 +0200

    Added amount to Price function with one currency type.

--- src/finance/price.jl
+++ src/finance/price.jl
@@ -11,8 +11,8 @@ struct Price
     Price(components::Dict{BalanceEntry, <:Real}, main_currency::BalanceEntry) = new(Dict{BalanceEntry, Currency}(components), main_currency)
 end
 
-function Price(main_currency::BalanceEntry)
-    Price(Dict{BalanceEntry, Currency}(), main_currency)
+function Price(main_currency::BalanceEntry, amount::Real)
+    Price(Dict{BalanceEntry, Currency}(main_currency => amount), main_currency)
 end
 
 function Price(components::AbstractVector{<:Pair{BalanceEntry, <:Real}}, main_currency::BalanceEntry = components[1][1])
