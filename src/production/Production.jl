61217f9643a5cf04da46013360a5fa13634b0545
Stef Kuypers <stef@happonomy.org>
2022-09-14 11:46:24 +0200

    Added PhysicalStock and InfiniteStock to exports

--- src/production/Production.jl
+++ src/production/Production.jl
@@ -18,7 +18,7 @@ include("entity.jl")
 export health, damaged, usable, restorable, use!, restore!, maintenance_due, maintain!, damage!, decay!, destroy!, destroyed
 
 include("stock.jl")
-export Stock
+export Stock, PhysicalStock, InfiniteStock
 export current_stock, has_stock, stocked, overstocked, add_stock!, retrieve_stock!, min_stock, min_stock!, max_stock, max_stock!, stock_limits, stock_limits!, purge!
 
 include("products.jl")
