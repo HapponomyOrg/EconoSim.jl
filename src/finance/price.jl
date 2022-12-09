898937097cdffa4a9adcbeb4a9ce7b3467b42e62
Stef Kuypers <stef@happonomy.org>
2022-12-09 11:50:22 +0100

    Fixed potential InexactError in purchases_available()

--- src/finance/price.jl
+++ src/finance/price.jl
@@ -96,7 +96,7 @@ function purchases_available(balance::Balance, price::Price, units::Integer; exc
         end
     end
 
-    return Integer(round(Float64(min(units, max_available), RoundDown)))
+    return Integer(round(Float64(min(units, max_available)), RoundDown))
 end
 
 """
