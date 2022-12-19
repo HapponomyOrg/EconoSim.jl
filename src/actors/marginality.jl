b7681cb0c218242ce0bf0a5a9e87cae4d323651e
Stef Kuypers <stef@happonomy.org>
2022-12-19 10:02:39 +0100

    Added mrginality process function that takes a Vector

--- src/actors/marginality.jl
+++ src/actors/marginality.jl
@@ -3,7 +3,7 @@ using DataStructures
 """
     Marginality - a progressive marginal use structure.
 
-* marginality::Vector{Tuple{Int64, Percentage}} - a sorted list of tuples consisting of a number of units and a probability resulting in an extra unit if the number of available units is below the indicated number.
+* marginality::SortedSet{Tuple{Int64, Percentage}} - a sorted list of tuples consisting of a number of units and a probability resulting in an extra unit if the number of available units is below the indicated number.
 """
 Marginality = SortedSet{Tuple{Int64, Percentage}}
 
@@ -46,4 +46,8 @@ function process(marginality::Marginality, units::Int64 = 0)
     end
 
     return total_units - units
+end
+
+function process(marginality::Vector{Tuple{Int64, Percentage}}, units::Int64 = 0)
+    return process(Marginality(marginality), units)
 end
