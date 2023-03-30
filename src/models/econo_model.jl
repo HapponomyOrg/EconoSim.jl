6d1de3575cc6e162d9ab026575c9c0037d0e3f8c
Stef Kuypers <stef@happonomy.org>
2023-03-30 13:51:09 +0200

    Moved step update to econo_step!

--- src/models/econo_model.jl
+++ src/models/econo_model.jl
@@ -21,7 +21,7 @@ end
 function create_properties(model_behaviors::Union{Nothing, Function, Vector{Function}})
     properties = Dict{Symbol, Any}()
     properties[:id_counter] = 0
-    properties[:step] = -1
+    properties[:step] = 1
     properties[:model_behaviors] = behavior_vector(model_behaviors)
 
     return properties
@@ -48,8 +48,7 @@ function next_id(model::ABM)
 end
 
 function add_actor(model::ABM, actor::AbstractActor)
-    actor.id = model.id_counter
-    model.id_counter += 1
+    actor.id = next_id(model)
     add_agent!(actor, model)
 
     return actor
@@ -63,8 +62,6 @@ clear_model_behaviors(model) = (empty!(model.model_behaviors); model)
 get_step(model) = model.step
 
 function econo_model_step!(model::ABM)
-    model.step += 1
-
     for behavior in model.model_behaviors
         behavior(model)
     end
@@ -72,6 +69,7 @@ end
 
 function econo_step!(model::ABM, steps::Integer = 1, actors_first::Bool = false)
     step!(model, actor_step!, econo_model_step!, steps, actors_first)
+    model.step += steps
 end
 
 function run_econo_model!(model::ABM, steps::Integer, actors_first = false; kwargs...)
