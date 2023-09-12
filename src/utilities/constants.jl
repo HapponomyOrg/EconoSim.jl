272e11ae67bc12684c7d8e81cba69a32bf29f3cf
Stef Kuypers <stef@happonomy.org>
2023-09-12 17:32:28 +0200

    Made changes to support flexible Currency precision

--- src/utilities/constants.jl
+++ src/utilities/constants.jl
@@ -1,4 +1,4 @@
 INF = typemax(Int64) # Indicates infinity for Int64
-CUR_MAX = typemax(Currency)
-CUR_MIN = typemin(Currency)
-CUR_0 = Currency(0)
+CUR_MAX() = typemax(Currency)
+CUR_MIN() = typemin(Currency)
+CUR_0() = Currency(0)
