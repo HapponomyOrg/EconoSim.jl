d189ce938cefa5b8c5bf45bd44827a604a13fee7
Stef Kuypers <stef@happonomy.org>
2023-09-12 17:35:30 +0200

    Added code to run all tests with different Currency precisions

--- test/run_tests.jl
+++ test/run_tests.jl
@@ -1,3 +1,16 @@
+using EconoSim
+
+set_currency_precision!(4)
+
+include("finance_test.jl")
+include("production_test.jl")
+include("utilities_test.jl")
+include("models_test.jl")
+include("actors_test.jl")
+
+# Testing whether everything works with other precision
+set_currency_precision!(6)
+
 include("finance_test.jl")
 include("production_test.jl")
 include("utilities_test.jl")
