227e6b540ebafccfc8d310ed5da15d2e6e08cdae
Stef Kuypers <stef@happonomy.org>
2023-12-07 11:16:31 +0100

    Added test

--- test/models_test.jl
+++ test/models_test.jl
@@ -12,6 +12,19 @@ using Agents
     @test !is_contribution_active(model)
 end
 
+@testset "Adding and deleting model behaviors" begin
+    model_behavior_function(model) = model
+
+    model = create_econo_model()
+
+    @test !has_model_behavior(model, model_behavior_function)
+    add_model_behavior!(model, model_behavior_function)
+    @test has_model_behavior(model, model_behavior_function)
+
+    delete_model_behavior!(model, model_behavior_function)
+    @test !has_model_behavior(model, model_behavior_function)
+end
+
 @testset "SuMSy contribution" begin
     # model = create_sumsy_model(SuMSy(2000, 0, 0.1, 10),
     #                             contribution_settings = SuMSy(0, 0, 0.2, 10),
