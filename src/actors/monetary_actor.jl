26f445c9c7a1919293b4a7e6fda44149ab589f61
Stef Kuypers <stef@happonomy.org>
2024-12-30 09:15:28 +0100

    Fixed type bug

--- src/actors/monetary_actor.jl
+++ src/actors/monetary_actor.jl
@@ -6,7 +6,8 @@ MonetaryActor - agent representing an actor that has a balance sheet.
 # Fields
 * balance::AbstractBalance - the balance sheet of the actor.
 
-After creation, any field can be set on the actor, even those which are not part of the structure. This can come in handy when when specific state needs to be stored with the actor.
+After creation, any field can be set on the actor, even those which are not part of the structure.
+This can come in handy when when specific state needs to be stored with the actor.
 """
 @agent struct MonetaryActor{C}(Actor) <: BalanceActor{C}
     model::ABM
@@ -17,8 +18,8 @@ end
 
 function create_monetary_actor(model::ABM;
                                 balance::AbstractBalance = Balance(),
-                                income::Currency = CUR_0,
-                                expenses::Currency = CUR_0,                           
+                                income::Real = 0,
+                                expenses::Real = 0,                           
                                 types::Set{Symbol} = Set{Symbol}(),
                                 behaviors::Vector{Function} = Vector{Function}())
     return MonetaryActor{Currency}(model,
