b0628003e7ae40aaa6a67818280f01e3a02ca8ec
Stef Kuypers <stef@happonomy.org>
2025-01-23 12:52:28 +0100

    Updated

--- test/models_test.jl
+++ test/models_test.jl
@@ -16,12 +16,18 @@ using Agents
 end
 
 @testset "Registering SuMSy GI as income" begin
-    model = create_sumsy_model(1, register_gi_as_income = true, model_behaviors = process_model_sumsy!)
+    model = create_sumsy_model(sumsy_interval = 30, register_gi_as_income = true, model_behaviors = process_model_sumsy!)
     @test abmproperties(model)[:register_gi_as_income]
 
     sumsy = SuMSy(1000, 0, 0.1)
-    actor = add_sumsy_actor!(model, sumsy = sumsy, sumsy_interval = 1)
+    actor = add_sumsy_actor!(model, sumsy = sumsy, initialize = false)
 
-    run_econo_model!(model, 1)
+    data = run_econo_model!(model, 1, adata = [:income], mdata = [:total_gi, :total_demurrage], init = true)
+    @test actor.income == 0
+
+    data = run_econo_model!(model, 29, adata = [:income], mdata = [:total_gi, :total_demurrage], init = true)
+    @test actor.income == 1000
+
+    data = run_econo_model!(model, 30, adata = [:income], mdata = [:total_gi, :total_demurrage], init = true)
     @test actor.income == 1900
 end
