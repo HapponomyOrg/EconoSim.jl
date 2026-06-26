cb350c87953861324858f10a7dcc2c586684c605
Stef Kuypers <stef@happonomy.org>
2026-06-26 13:17:34 -0500

    Added activate_actors! to export

--- src/models/Models.jl
+++ src/models/Models.jl
@@ -1,6 +1,6 @@
 include("econo_model.jl")
 export create_econo_model, create_unremovable_econo_model
-export add_actor!
+export add_actor!, activate_actors!
 export econo_step!, econo_model_step!, run_econo_model!
 export has_model_behavior, add_model_behavior!, delete_model_behavior!, clear_model_behaviors
 export get_step
